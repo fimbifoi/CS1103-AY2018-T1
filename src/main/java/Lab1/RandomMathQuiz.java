fa53fd5d6022f27ce2c6bb9990c6a9c873986170
AlexTingaev <tingaev@gmail.com>
2017-09-07 13:32:18 +0300

    implemented Talker logic

--- src/main/java/Lab1/RandomMathQuiz.java
+++ src/main/java/Lab1/RandomMathQuiz.java
@@ -3,15 +3,20 @@ package Lab1;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomMathQuiz {
     public static void main(String[] args) {
 
         Talker talker = new Talker();
+        talker.prepareQuiz();
 
         QuestionProducer producer = new QuestionProducer();
-        Map<String, Integer> questions = producer.makeRandomQuestions(10);
+        Map<String, Integer> questions = producer.makeRandomQuestions(talker.getCountOfQuestions());
+
+        talker.doQuiz(questions);
+
 
 
     }
@@ -44,7 +49,7 @@ public class RandomMathQuiz {
                         answer = value1 / value2;
                         break;
                 }
-                example = String.format("%d %s %d", value1, sign, value2);
+                example = String.format("%d %s %d = ", value1, sign, value2);
                 map.put(example, answer);
             }
                 return map;
@@ -53,5 +58,63 @@ public class RandomMathQuiz {
     }
 
     private static class Talker {
+        String name;
+        Double score;
+        Boolean enough;
+        Scanner scanner;
+
+        int countOfQuestions;
+
+        public void prepareQuiz() {
+
+            System.out.print("Hello. This is quiz for check your basic arithmetic skills. Please, enter your name: ");
+            name = scanner.nextLine();
+            System.out.println();
+            System.out.printf("%s, Now tell me please count of question that you ready to solve: ", name);
+            countOfQuestions = scanner.nextInt();
+            if (countOfQuestions < 1) {
+                System.out.print("please, enter correct count of questions (1 or more): ");
+                countOfQuestions = scanner.nextInt();
+            }
+        }
+
+        public Talker() {
+            name = "";
+            score = 0.0;
+            enough = false;
+            scanner = new Scanner(System.in);
+            countOfQuestions = 0;
+        }
+
+        public int getCountOfQuestions() {
+            return countOfQuestions;
+        }
+
+        public void doQuiz(Map<String, Integer> questions) {
+            while (!enough) {
+                for (Map.Entry<String, Integer> entry : questions.entrySet()) {
+                    System.out.print(entry.getKey());
+                    if (entry.getValue() == scanner.nextInt()) {
+                        score++;
+                        System.out.println("correct! next:");
+                    }
+                    else {
+                        System.out.print("incorrect. please, try again: " + entry.getKey());
+                        if (entry.getValue() == scanner.nextInt()) {
+                            System.out.println("correct witch the second try. next: ");
+                            score = score + 0.5;
+                        }
+                        else System.out.println("incorrect again. next: ");
+                    }
+                }
+                System.out.printf("well done. your result is %.2f of %d possible.\n", score, questions.size());
+                score = 0.0;
+                System.out.println("want to play again? (y/n)");
+                if (scanner.next().equalsIgnoreCase("n")) enough = true;
+
+            }
+
+        }
     }
+
 }
